5e8b38db48a8c402593b5794b872bb9115342ad9
nitomoki <nitomoki.2@gmail.com>
2020-02-27 04:46:08 +0900

    bash scripts update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# variables {{{
 softwares=(
     curl
     make
@@ -24,12 +25,65 @@ softwares=(
     nodejs
     npm
     pip3
+    clang-tools-8
 )
 
-softwares_pip=(
+softwares_pip3=(
     msgpack
+    neovim
 )
+# }}}
 
-for software in "${softwares[@]}"; do
-    sudo apt install $software || sudo apt upgrade $software
-done
+# functions {{{
+function install_neovim(){
+    echo "neovim install start..."
+    git clone https://github.com/neovim/neovim /$HOME/neovim
+    cd /$HOME/neovim
+    make CMAKE_BUILD_TYPE=Release 
+    sudo make install 
+    cd /$HOME 
+    echo "done"
+}
+
+function install_nerdfonts(){
+    echo "nerd-fonts install start..."
+    git clone https://github.com/ryanoasis/nerd-fonts.git /$HOME/nerd-fonts 
+    cd /$HOME/nerd-fonts 
+    ./install.sh
+    cd /$HOME 
+    rm -rf nerd-fonts
+    echo "done"
+}
+
+function install_apt-softwares(){
+    echo "apt install start..."
+    sudo apt update -y
+    for software in "${softwares[@]}"; do
+        sudo apt install -y $software || sudo apt update $software
+    done
+    echo "done"
+}
+
+function install_pip3-softwares(){
+    echo "pip3 install start..."
+    sudo pip3 update -y
+    for software_pip3 in "${softwares_pip3[@]}"; do
+        sudo pip3 install -y $software_pip3 || sudo pip3 update
+    done
+    echo "done"
+}
+
+
+# }}}
+
+
+# installing
+install_apt
+install_pip3
+install_neovim
+install_nerdfonts
+
+echo "clangd setting"
+sudo update-altanatives --install /usr/bin/clangd clangd usr/bin/clangd-8 100
+
+
