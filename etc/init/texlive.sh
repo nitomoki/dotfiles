9e0527a6f71dc649cdcaa4c92dc5bfaf1f4fcb26
nitomoki <nitomoki.2@gmail.com>
2020-09-14 19:11:09 +0900

    add texlive

---
+++ etc/init/texlive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install texlive-latex-recommended \
+   texlive-latex-extra \
+   texlive-fonts-recommended \
+   texlive-fonts-extra \
+   texlive-lang-japanese \
+   texlive-lang-cjk
