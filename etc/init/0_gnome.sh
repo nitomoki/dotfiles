b8f956a993e56932dcc549951f1a54ce1fda37de
nitomoki <nitomoki.2@gmail.com>
2020-09-14 16:30:01 +0900

    caps to ctrl

--- etc/init/0_gnome.sh
+++ etc/init/0_gnome.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# hide gnome-dock
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide false && gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false && gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
+
+# chage default dir name to Eng
 LANG=C xdg-user-dirs-gtk-update
+
+# change caps-lock to ctrl
+gsettings set org.gnome.libgnomekbd.keyboard options "['ctrl\tctrl:nocaps']"
