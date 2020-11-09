f711d96aa9becc723ef7cb3b529d7b30e2bf6211
nitomoki <nitomoki.2@gmail.com>
2020-11-09 17:36:09 +0900

    edit quickrun

--- bin/ros_noetic_install.sh
+++ bin/ros_noetic_install.sh
@@ -3,9 +3,10 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
-sudo apt update
-sudo apt -y install ros-noetic-desktop-full
-sudo apt -y install python-rosinstall
+sudo apt update  -y
+sudo apt install -y ros-noetic-desktop-full
+sudo apt install -y python-rosinstall
+sudo apt install -y ros-noetic-joystick-drivers ros-noetic-image-proc
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
@@ -15,3 +16,4 @@ catkin_make
 
 echo "source /opt/ros/noetic/setup.zsh" >> ~/.zshrc.local
 source ~/.zshrc
+
