343bd949502950828b68dc71c6e9ea9e289342f0
nitomoki <nitomoki.2@gmail.com>
2021-08-31 04:14:21 +0900

    del: .zsh_history

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -16,7 +16,7 @@ fi
 autoload -Uz colors && colors
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
-export HISTFILE=$HOME/dotfiles/zsh/.zsh_history
+export HISTFILE=$HOME/.zsh_history
 export HISTSIZE=1000000
 export SAVEHIST=1000000
 setopt EXTENDED_HISTORY
