aeb1e0bde28a2b5c269fe6bc62a1eb0cb8e90d62
Tomoki Nishino <nitomoki.2@gmail.com>
2022-02-01 12:28:27 +0900

    change: vsnip to luasnip

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -40,6 +40,8 @@ return require'packer'.startup(function(use)
                     {'hrsh7th/cmp-calc'},
                     {'hrsh7th/cmp-path'},
                     {'hrsh7th/cmp-cmdline'},
+                    {'L3MON4D3/LuaSnip'},
+                    {'saadparwaiz1/cmp_luasnip'}
                 },
         config = function()
             require'config.cmp'
