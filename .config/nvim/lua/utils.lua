f47d7f000bd937df7f8345990144a73c1977e1cf
Tomoki Nishino <nitomoki.2@gmail.com>
2022-02-25 07:11:16 +0900

    del: unused utils

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,43 +1,14 @@
 local M = {}
-local cmd = vim.cmd
 
 function M.create_augroup(autocmds, name)
-    cmd('augroup ' .. name)
-    cmd('autocmd!')
+    vim.cmd('augroup ' .. name)
+    vim.cmd('autocmd!')
     for _, autocmd in ipairs(autocmds) do
-        cmd('autocmd ' .. table.concat(autocmd, ' '))
+        vim.cmd('autocmd ' .. table.concat(autocmd, ' '))
     end
-    cmd('augroup END')
+    vim.cmd('augroup END')
 end
 
-
-function M.add_rtp(path)
-    local rtp = vim.o.rtp
-    vim.o.rtp = rtp .. ',' .. path
-end
-
-function M.map(mode, keys, action, options)
-    if options == nil then
-        options = {noremap = true, silent = true}
-    end
-    vim.api.nvim_set_keymap(mode, keys, action, options)
-end
-
-function M.map_lua(mode, keys, action, options)
-    if options == nil then
-        options = {noremap = true, silent = true}
-    end
-    vim.api.nvim_set_keymap(mode, keys, "<cmd>lua " .. action .. "<CR>", options)
-end
-
-
-function M.map_buf(mode, keys, action, options, buf_nr)
-    if options == nil then
-        options = {}
-    end
-    local buf = buf_nr or 0
-    vim.api.nvim_set_keymap(buf, mode, keys, "<cmd>lua " .. action .. "<CR>", options)
-end
 
 function M.is_plugin_installed(plugin)
     return not (vim.fn.globpath(vim.o.rtp, 'pack/*/*/' .. plugin, 1) == '')
@@ -52,6 +23,7 @@ function M.get_buf_byte()
         return byte - 1
     end
 end
+
 
 _G.utils = M
 return M
