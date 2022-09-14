5add25a30e09afba7b948b35e86e043e8b2aba10
Tomoki Nishino <nitomoki.2@gmail.com>
2022-09-14 14:19:40 +0900

    change: telescope keymap

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -49,16 +49,14 @@ require("telescope").setup {
     },
 }
 
-local s_opts = { noremap = true, silent = true }
-local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 local builtin = require "telescope.builtin"
 local extensions = require("telescope").extensions
-keymap.set("n", "<leader>b", builtin.buffers, s_opts)
-keymap.set("n", "<leader>g", builtin.live_grep, s_opts)
-keymap.set("n", "<leader>r", builtin.resume, s_opts)
-keymap.set("n", "<leader>fb", extensions.file_browser.file_browser, s_opts)
-keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, s_opts)
-keymap.set("n", "<leader>ff", builtin.find_files, s_opts)
-keymap.set("n", "<leader>fh", builtin.help_tags, s_opts)
-keymap.set("n", "<leader>fo", builtin.oldfiles, s_opts)
-keymap.set("n", "<leader>fr", builtin.registers, s_opts)
+vim.keymap.set("n", "<leader>b", builtin.buffers, opts)
+vim.keymap.set("n", "<leader>r", builtin.resume, opts)
+vim.keymap.set("n", "<leader>fb", extensions.file_browser.file_browser, opts)
+vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, opts)
+vim.keymap.set("n", "<leader>ff", builtin.find_files, opts)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, opts)
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles, opts)
