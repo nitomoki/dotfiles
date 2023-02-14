b9aa05b148dbb4ac07f2fcd1cff93eb36d3aa037
Tomoki Nishino <nitomoki.2@gmail.com>
2023-02-14 12:02:51 +0900

    del: bin

--- bin/texlab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/latex-lsp/texlab /$HOME/texlab
-cd /$HOME/texlab
-sudo cargo build --release
-cd /$HOME
--- bin/texlive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y texlive-latex-recommended \
-    texlive-latex-extra \
-    texlive-fonts-recommended \
-    texlive-fonts-extra \
-    texlive-lang-japanese \
-    texlive-lang-cjk \
-    texlive-science
